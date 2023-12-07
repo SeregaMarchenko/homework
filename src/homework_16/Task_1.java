75497c025e1eb47560b94160f9a7218bdd383479
SeregaMarchenko <mrchnksrg1@gmail.com>
2023-12-07 17:15:47 +0300

    add homework_16

---
+++ src/homework_16/Task_1.java
@@ -0,0 +1,20 @@
+package homework_16;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Random;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Random random = new Random();
+        for(int i=0;i<20;i++){
+            arrayList.add(random.nextInt(10));
+        }
+        Optional<Integer> sum = arrayList.stream()
+                .distinct()
+                .filter(number->number%2==0)
+                .reduce(Integer::sum);
+        System.out.println("Сумма равна "+sum.get());
+    }
+}
---
+++ src/homework_16/Task_2.java
@@ -0,0 +1,38 @@
+package homework_16;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Task_2 {
+
+    public static final String[] NAMES = new String[]{
+            "Oleg", "Max", "Alina",
+            "Karina", "Milana", "Sergey",
+            "Stanislav", "Zah", "Polina",
+            "Angelina", "Marina", "Vlad",
+            "Dak", "Viktoria", "Vitia"};
+
+
+    public static void main(String[] args) {
+        HashSet<Integer> correctId = new HashSet<>();
+        correctId.add(1);
+        correctId.add(2);
+        correctId.add(5);
+        correctId.add(8);
+        correctId.add(9);
+        correctId.add(13);
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        for (int i = 0; i < NAMES.length; i++) {
+            hashMap.put(i + 1, NAMES[i]);
+        }
+        ArrayList<String> names = (ArrayList<String>) hashMap.entrySet().stream()
+                .filter(entry->correctId.contains(entry.getKey())&&entry.getValue().length()%2!=0)
+                .peek(entry-> new StringBuilder(entry.getValue()).reverse())
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+        System.out.println(names);
+    }
+}
